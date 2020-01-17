5c78118701544be588bf3f4b6375dea3f4630dbe
Andre Madarang <andre_madarang@hotmail.com>
2020-01-16 22:13:39 -0500

    Working

---
+++ resources/js/components/Kustomer/Partials/Popup.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="kustomer-popup">
+        <kustomer-header>
+            <img slot="logo" :src="params.logo">
+            <h1 slot="title"
+                v-text="labels.title"
+                :style="{'color': params.colors.primary}"
+            ></h1>
+        </kustomer-header>
+        <div class="kustomer-container">
+            <kustomer-feedbacks-list
+                :feedbacks="params.feedbacks"
+                :labels="labels"
+                @selected="setSelectedFeedback"
+            ></kustomer-feedbacks-list>
+            <kustomer-feedback-form
+                :feedback="selectedFeedback"
+                :params="params"
+                :labels="labels"
+                @unselected="selectedFeedback = undefined"
+            ></kustomer-feedback-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['params', 'labels'],
+
+    data() {
+        return {
+            selectedFeedback: undefined,
+        }
+    },
+
+    methods: {
+        setSelectedFeedback(feedback) {
+            this.selectedFeedback = feedback;
+        }
+    },
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,39 +62,49 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link rel="stylesheet" href="/css/app.css">
+        {{-- <script src="{{ asset('vendor/kustomer/js/kustomer.js') }}" defer></script> --}}
+        <script src="/js/app.js" defer></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div id="app">
+            <div class="flex-center position-ref full-height">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="content">
+                    <div class="title m-b-md">
+                        Laravel
+                    </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="links">
+                        <a href="https://laravel.com/docs">Docs</a>
+                        <a href="https://laracasts.com">Laracasts</a>
+                        <a href="https://laravel-news.com">News</a>
+                        <a href="https://blog.laravel.com">Blog</a>
+                        <a href="https://nova.laravel.com">Nova</a>
+                        <a href="https://forge.laravel.com">Forge</a>
+                        <a href="https://vapor.laravel.com">Vapor</a>
+                        <a href="https://github.com/laravel/laravel">GitHub</a>
+                    </div>
                 </div>
             </div>
+            {{-- @include('kustomer::kustomer') --}}
+            <kustomer
+                :params="{{ json_encode(config('kustomer')) }}"
+                :labels="{{ json_encode(trans('kustomer::kustomer')) }}"
+            ></kustomer>
         </div>
     </body>
 </html>
